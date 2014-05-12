f6f69f1970bba16a7b031d6db1ada1cfe6b6ddb2
Antonis Kalou <kalouantonis@gmail.com>
2014-05-12 15:35:43 +0300

    Comments, i like comments

--- tools/GuidGen.py
+++ tools/GuidGen.py
@@ -10,7 +10,8 @@ except ValueError:
 	print "No arguments passed.\n\tUsage: GuidGen.py <name>"
 	sys.exit()
 
-# Generate UUID using 
+# Generate UUID using given name
 guid = str(uuid.uuid3(uuid.NAMESPACE_OID, name))
 
+# Output 1 byte hex identifier
 print name + ": " + "0x" + guid[:8]
