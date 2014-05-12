d1c4415e6bd1577bcb4827e7036a9f887189295a
Antonis Kalou <kalouantonis@gmail.com>
2014-05-12 15:15:33 +0300

    Made ConcurrentQueue header only

--- core/src/DataStructures/ConcurrentQueue.h
+++ core/src/DataStructures/ConcurrentQueue.h
@@ -4,6 +4,7 @@
 #include <queue>
 #include <mutex>
 #include <condition_variable>
+#include <thread>
 
 template <typename T>
 class ConcurrentQueue
@@ -13,13 +14,51 @@ public:
      * @brief Pop off the front of the queue
      * @return Item in the front of queue
      */
-    T pop();
+    T pop()
+    {
+        // Lock queue
+        std::unique_lock<std::mutex> lock(m_mutex);
+        while(m_queue.empty())
+        {
+            // Block current thread until notification is received
+            // Also releases lock to other waiting threads
+            m_conditionVar.wait(lock);
+        }
 
-    void push(const T& item);
+        // Get item off front
+        auto item = m_queue.front();
+        // Remove front item from queue
+        m_queue.pop();
+        return item;
+    }
+
+    bool empty()
+    {
+        std::unique_lock<std::mutex> lock(m_mutex);
+
+        return m_queue.empty();
+    }
+
+    void push(const T& item)
+    {
+        std::unique_lock<std::mutex> lock(m_mutex);
+        m_queue.push(item);
+        lock.unlock();
+        // Notify one waiting thread
+        m_conditionVar.notify_one();
+    }
+
     /**
      * @brief Push R-Value reference. Used for std correctness
      */
-    void push(T&& item);
+    void push(T&& item)
+    {
+        std::unique_lock<std::mutex> lock(m_mutex);
+        // Transfer ownership
+        m_queue.push(std::move(item));
+        lock.unlock();
+        m_conditionVar.notify_one();
+    }
 
 private:
     std::queue<T> m_queue;
